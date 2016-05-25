45e4bb06d3c457026c72ea3134104ee914497c34
Eva <Bresciano>
2016-05-25 09:34:37 -0600

    stretch

---
+++ stretch problem 8.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func alarmClock(number: Int) -> Int {
+    
+    let number = Int(257)
+    
+    return number
+}
+
